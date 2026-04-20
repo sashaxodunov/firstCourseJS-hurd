b7a855de2006342703b7909c2b46d4698dd73a90
sashaxodunov <sasha@xodunov.ru>
2026-04-20 22:47:31 +0300

    Урок03(Усложненное задание). Динамическая типизация данных в Javascript. Условия, ветвления

--- script.js
+++ script.js
@@ -15,3 +15,70 @@ console.log(result);
 
 const firstTwo = String(result).slice(0, 2);
 console.log(Number(firstTwo));
+
+/*
+ * Урок 03.
+ * Динамическая типизация данных в Javascript. Условия, ветвления.
+ * Усложненное задание
+ */
+
+// Задание 1
+
+let lang = "ru";
+
+if (lang === "ru") {
+  console.log(
+    "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье",
+  );
+} else if (lang === "en") {
+  console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+}
+
+switch (lang) {
+  case "ru":
+    console.log(
+      "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье",
+    );
+    break;
+  case "en":
+    console.log(
+      "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday",
+    );
+    break;
+}
+
+let days = [
+  [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+  ],
+  [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ],
+];
+
+let index = lang === "en" ? 1 : 0;
+
+console.log(days[index]);
+
+// Задание 2
+
+let namePerson = "Артем";
+
+let status =
+  namePerson === "Артем"
+    ? console.log("Директор")
+    : namePerson === "Александр"
+      ? console.log("Преподаватель")
+      : console.log("Студент");

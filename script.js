67e6c403217254bb55a8e010a49e0d9e8d7205d0
sashaxodunov <sasha@xodunov.ru>
2026-04-24 14:11:57 +0300

    Урок 5. Усложненное задание 1.2

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const arr = ["4168464", "6488464", "14648648", "25463854566", "4358463854", "654
 const filter = arr.filter(s => s.startsWith("2") || s.startsWith("4"));
 
 console.log(filter); // выводим новый массив
+
+for (i = 1; i <= 100; i++) {
+  if (i < 2) continue; // 1 не является простым
+  let isPrime = true;
+
+  for (let j = 2; j * j <= i; j++) {
+    if (i % j === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+     console.log(i + " Делители этого числа: 1 и " + i);
+  }
+}

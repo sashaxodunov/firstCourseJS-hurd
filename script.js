9113768a74e410ff648e42bc5cbf3a073902cd50
sashaxodunov <sasha@xodunov.ru>
2026-04-29 23:21:38 +0300

    Урок 11. Задание 4

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const books = document.querySelectorAll(".book");
+const title = document.querySelectorAll("h2");
+const adv = document.querySelectorAll(".adv");
 books[0].before(books[1]);
 books[2].before(books[4]);
 books[2].before(books[3]);
@@ -8,7 +10,9 @@ document.body.style.backgroundImage = "url('image/you-dont-know-js.jpg')";
 
 console.log(books);
 
-const title = document.querySelectorAll("h2");
 title[2].innerHTML = "<a>Книга 3. this и Прототипы Объектов</a>";
 
+adv[0].remove();
+
 console.log(title);
+console.log(adv);

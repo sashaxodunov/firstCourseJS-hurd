3630ecbbef50a046e817610a7be246090e28d1cc
sashaxodunov <sasha@xodunov.ru>
2026-05-01 06:47:14 +0300

    Урок 11. Задание 1

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const button = document.getElementById("btn");
+const text = document.getElementById("text");
+const square = document.getElementById("square");
 
-
-
-
-
+button.addEventListener("click", function () {
+  const color = text.value;
+  square.style.backgroundColor = color;
+});

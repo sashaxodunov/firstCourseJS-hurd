d7a8fc258af88e7b126725d4b8ffdeb05fd6a021
sashaxodunov <sasha@xodunov.ru>
2026-05-01 07:00:48 +0300

    Урок 11. Задание 3

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const button = document.getElementById("btn");
 const text = document.getElementById("text");
 const square = document.getElementById("square");
 const eBtn = document.getElementById("e_btn");
+const range = document.getElementById("range");
+const circle = document.getElementById("circle");
 
 button.addEventListener("click", function () {
   const color = text.value;
@@ -9,5 +11,9 @@ button.addEventListener("click", function () {
 });
 
 eBtn.style.display = "none";
-console.log(eBtn);
-console.dir(eBtn);
+
+range.addEventListener("input", () => {
+  const value = range.value;
+  circle.style.width = value + "%";
+  circle.style.height = value + "%";
+});

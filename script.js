04e049fa5d05d1ff2e5f07818e1725e5c5fdc958
sashaxodunov <sasha@xodunov.ru>
2026-05-05 18:37:36 +0300

    Урок 16. Задание 1- 3

---
+++ script.js
@@ -0,0 +1,14 @@
+class First {
+  hello() {
+    console.log("Привет я метод родителя!");
+  }
+}
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log("А я наследуемый метод!");
+  }
+}
+
+const obj = new Second();
+obj.hello();
